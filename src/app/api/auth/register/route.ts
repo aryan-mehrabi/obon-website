78a6ccfbd71e45e4677b9a952645b2adee12737b
aryan <aryan.me77@yahoo.com>
2023-10-18 18:54:53 +0330

    feat: add register router

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,60 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../[...nextauth]/route";
+import prisma from "@/prisma/client";
+import * as z from "zod";
+import { credentialsSchema } from "@/lib/validations/auth";
+import { hashPassword } from "@/lib/helpers";
+import { redirect } from "next/navigation";
+
+export async function POST(request: Request) {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/");
+
+  const data = await request.json();
+
+  try {
+    credentialsSchema.parse(data);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: error.issues.reduce(
+            (error, { message }) => error + "\n " + message,
+            ""
+          ),
+        }),
+        {
+          status: 400,
+        }
+      );
+    }
+  }
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (user) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: "user already exists",
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+
+  //actual registering
+  const newUser = await prisma.user.create({
+    data: {
+      email: data.email,
+      password: hashPassword(data.password),
+    },
+  });
+
+  return Response.json({ success: true, data: newUser });
+}
