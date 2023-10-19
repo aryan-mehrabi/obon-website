1b7b8ba0e9921781b4e3c2bd2fe302d904a6db67
aryan <aryan.me77@yahoo.com>
2023-10-20 02:35:17 +0330

    feat: exclude static file in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,6 @@ import { getLocale, i18n } from "./lib/locale";
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
-  // // If you have one
-  // if (
-  //   [
-  //     '/manifest.json',
-  //     '/favicon.ico',
-  //     // Your other files in `public`
-  //   ].includes(pathname)
-  // )
-  //   return
-
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
     locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
@@ -37,5 +26,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
 };
