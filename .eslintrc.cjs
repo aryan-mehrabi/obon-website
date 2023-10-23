1ec60c0d448984feffd68836708d318afe20656a
aryan <aryan.me77@yahoo.com>
2023-10-23 15:07:45 +0330

    feat: add airbnb eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,48 +1,50 @@
 module.exports = {
-	extends: [
-		"eslint:recommended",
-		"plugin:typescript-sort-keys/recommended",
-		"plugin:react/jsx-runtime",
-		"plugin:react-hooks/recommended",
-		"next/core-web-vitals",
-	],
-	overrides: [
-		{
-			extends: [
-				"plugin:@typescript-eslint/recommended",
-				"plugin:@typescript-eslint/recommended-requiring-type-checking",
-			],
-			files: ["**/*.{ts,tsx}"],
-			parserOptions: {
-				tsconfigRootDir: __dirname,
-				project: ["./tsconfig.json"],
-			},
-		},
-		{
-			files: "*.json",
-			parser: "jsonc-eslint-parser",
-			rules: {
-				"jsonc/sort-keys": "error",
-			},
-			extends: ["plugin:jsonc/recommended-with-json"],
-		},
-	],
-	parser: "@typescript-eslint/parser",
-	plugins: [
-		"@typescript-eslint",
-		"react",
-		"react-hooks",
-		"simple-import-sort",
-		"typescript-sort-keys",
-	],
-	root: true,
-	rules: {
-		"simple-import-sort/exports": "error",
-		"simple-import-sort/imports": "error",
-	},
-	settings: {
-		react: {
-			version: "detect",
-		},
-	},
+  extends: [
+    "eslint:recommended",
+    "plugin:typescript-sort-keys/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "next/core-web-vitals",
+    "airbnb",
+  ],
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["**/*.{ts,tsx}"],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+      },
+    },
+    {
+      files: "*.json",
+      parser: "jsonc-eslint-parser",
+      rules: {
+        "jsonc/sort-keys": "error",
+      },
+      extends: ["plugin:jsonc/recommended-with-json"],
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "simple-import-sort",
+    "typescript-sort-keys",
+  ],
+  root: true,
+  rules: {
+    "simple-import-sort/exports": "error",
+    "simple-import-sort/imports": "error",
+    quotes: ["error", "double", { avoidEscape: true }],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 };
