1c1ec827b145a519e4a0dabe75556f001a10d7f4
aryan <aryan.me77@yahoo.com>
2023-11-05 02:42:36 +0330

    feat: add desktop

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -22,7 +22,7 @@ export default async function Home({
     <div className="flex flex-col">
       <header className="bg-[url('/header-image.jpg')] bg-cover overflow-auto">
         <Navbar navMenu={navMenu} />
-        <div className="bg-white backdrop-blur-md bg-opacity-80 my-64 mx-3 text-center p-10">
+        <div className="bg-white backdrop-blur-md bg-opacity-80 my-60 mx-3 text-center p-10 md:max-w-3xl md:mx-auto">
           <h1 className="text-4xl leading-tight font-semibold mb-3">
             🌱
             <br />
@@ -31,18 +31,18 @@ export default async function Home({
           <p className="text-[18px]">{header.description}</p>
           <button
             type="button"
-            className="mt-9 w-full bg-eprimary p-2 text-white rounded-sm"
+            className="mt-9 bg-eprimary py-2 px-11 text-white rounded-sm"
           >
             {header.cta}
           </button>
         </div>
       </header>
-      <section className="my-16 mx-5">
+      <section className="my-16 mx-5 max-w-5xl md:mx-auto">
         <div className="text-center space-y-2 mb-14">
           <h2 className="text-4xl font-semibold">{product.title}</h2>
           <p className="text-[#5E6E89] text-xl">{product.description}</p>
         </div>
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col md:flex-row gap-5">
           <div className="shadow-sm rounded p-3 bg-white">
             <div className="h-[170px] overflow-hidden -mt-4 mb-2">
               <Image
@@ -50,6 +50,32 @@ export default async function Home({
                 width={3000}
                 height={2250}
                 src="/sample-product.png"
+              />
+            </div>
+            <p className="font-[500]">Spice mint</p>
+            <p className="rtl:text-left text-right">$9.99</p>
+          </div>
+          <div className="shadow-sm rounded p-3 bg-white">
+            <div className="h-[170px] overflow-hidden -mt-4 mb-2">
+              <Image
+                alt="sample product image"
+                width={3000}
+                height={2250}
+                src="/sample-product.png"
+                className=""
+              />
+            </div>
+            <p className="font-[500]">Spice mint</p>
+            <p className="rtl:text-left text-right">$9.99</p>
+          </div>
+          <div className="shadow-sm rounded p-3 bg-white">
+            <div className="h-[170px] overflow-hidden -mt-4 mb-2">
+              <Image
+                alt="sample product image"
+                width={3000}
+                height={2250}
+                src="/sample-product.png"
+                className=""
               />
             </div>
             <p className="font-[500]">Spice mint</p>
@@ -78,15 +104,15 @@ export default async function Home({
           </button>
         </div>
       </section>
-      <section className="my-16 space-y-10">
-        <div className="text-center mx-4 space-y-4">
+      <section className="my-16 space-y-10 max-w-5xl md:mx-auto md:grid md:grid-cols-2">
+        <div className="text-center mx-4 space-y-4 md:text-left md:rtl:text-right">
           <h2 className="font-bold text-4xl">{benefits.title}</h2>
           <p className="text-eprimary">{benefits.description}</p>
         </div>
-        <div>
+        <div className="row-span-2">
           <Image alt="" width={1380} height={964} src="/mockups.png" />
         </div>
-        <div className="mx-5">
+        <div className="mx-5 md:mx-auto">
           <ul className="list-image-[url('/checkmark-circle-outline.svg')] list-outside ps-8 text-xl flex flex-col gap-5">
             {benefits.benefit_list.map(({ title, description }) => (
               <li>
@@ -99,7 +125,7 @@ export default async function Home({
             ))}
           </ul>
         </div>
-        <div className="text-center">
+        <div className="text-center md:text-left md:rtl:text-right">
           <button
             type="button"
             className="bg-eprimary text-white px-11 py-2 rounded-sm"
@@ -108,28 +134,32 @@ export default async function Home({
           </button>
         </div>
       </section>
-      <footer className="bg-esecondary text-white py-10 px-4 relative">
-        <hr />
-        <div className="mt-4 mb-12">
-          <h3 className="text-3xl font-semibold mb-3">{footer.title}</h3>
-          <p>{footer.description}</p>
-        </div>
-        <div className="grid grid-cols-2 gap-y-10 mb-16">
-          {footer.menu.map(({ title, links }) => (
-            <div>
-              <h4 className="text-eprimary font-[500] mb-5">{title}</h4>
-              <ul className="flex flex-col gap-4">
-                {links.map(({ title: linkTitle, href }) => (
-                  <li>
-                    <Link href={href}>{linkTitle}</Link>
-                  </li>
-                ))}
-              </ul>
+      <footer className="bg-esecondary text-white relative">
+        <div className="py-10 px-4 max-w-5xl md:mx-auto">
+          <hr />
+          <div className="md:flex">
+            <div className="mt-4 mb-12">
+              <h3 className="text-3xl font-semibold mb-3">{footer.title}</h3>
+              <p>{footer.description}</p>
             </div>
-          ))}
-        </div>
-        <div className="bg-white text-black absolute w-full bottom-0 left-0 text-center p-4">
-          <p>{footer.bottom}</p>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-10 mb-16 md:mt-10">
+              {footer.menu.map(({ title, links }) => (
+                <div>
+                  <h4 className="text-eprimary font-[500] mb-5">{title}</h4>
+                  <ul className="flex flex-col gap-4">
+                    {links.map(({ title: linkTitle, href }) => (
+                      <li>
+                        <Link href={href}>{linkTitle}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="bg-white text-black absolute w-full bottom-0 left-0 text-center p-4">
+            <p>{footer.bottom}</p>
+          </div>
         </div>
       </footer>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,33 +18,34 @@ export default function Navbar({ navMenu }: PropTypes) {
   const ref = useRef<HTMLElement>(null);
   useClickOutside(ref, () => setIsMenuOpen(false));
   return (
-    <nav
-      ref={ref}
-      className="bg-white flex items-center justify-between p-7 fixed w-full z-10"
-    >
-      <HamburgerMenuIcon
-        onClick={() => setIsMenuOpen((val) => !val)}
-        className="w-8 h-8"
-      />
-      <h3 className="text-2xl font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        Candleaf
-      </h3>
-      <div
-        className={`absolute bottom-0 left-0 translate-y-full bg-white w-full ${
-          isMenuOpen ? "" : "hidden"
-        }`}
-      >
-        <ul className="px-10 py-5 flex flex-col gap-y-5 text-2xl">
-          {navMenu.map(({ title, href }) => (
-            <li>
-              <Link href={href}>{title}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="flex gap-3">
-        <PersonIcon className="w-8 h-8" />
-        <CartIcon className="w-8 h-8" />
+    <nav ref={ref} className="bg-white fixed w-full z-10">
+      <div className="flex items-center justify-between max-w-5xl md:mx-auto p-7 md:p-6">
+        <HamburgerMenuIcon
+          onClick={() => setIsMenuOpen((val) => !val)}
+          className="w-8 h-8 md:hidden"
+        />
+        <h3 className="text-2xl font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:static md:translate-x-0 md:translate-y-0">
+          Candleaf
+        </h3>
+        <div
+          className={`absolute bottom-0 left-0 translate-y-full bg-white w-full ${
+            isMenuOpen ? "" : "hidden"
+          } md:block md:static md:translate-y-0 md:w-auto`}
+        >
+          <ul className="px-10 py-5 flex flex-col md:flex-row gap-y-5 md:gap-16 text-2xl md:text-base md:p-0">
+            {navMenu.map(({ title, href }) => (
+              <li>
+                <Link className="p-2" href={href}>
+                  {title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex gap-3">
+          <PersonIcon className="w-8 h-8" />
+          <CartIcon className="w-8 h-8" />
+        </div>
       </div>
     </nav>
   );
