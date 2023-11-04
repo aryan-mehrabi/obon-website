40795b01e4e3dfe0652924bb58f826870661a743
aryan <aryan.me77@yahoo.com>
2023-11-04 21:03:39 +0330

    feat: add svgr to import svgs

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
