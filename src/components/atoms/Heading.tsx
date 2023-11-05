06ea4e40c561b9d1f1e480c535c7c59419d33cc7
aryan <aryan.me77@yahoo.com>
2023-11-06 02:42:15 +0330

    feat: add heading atom

---
+++ src/components/atoms/Heading.tsx
@@ -0,0 +1,38 @@
+import { createElement } from "react";
+
+const headerTypes = ["h1", "h2", "h3", "h4"] as const;
+interface PropTypes {
+  children: React.ReactNode;
+  className?: string;
+  type: (typeof headerTypes)[number];
+}
+
+export default function Heading({
+  children,
+  type,
+  className = "",
+  ...props
+}: PropTypes) {
+  let classes = "";
+  switch (type) {
+    case "h1":
+      classes = "text-4xl leading-tight";
+      break;
+    case "h2":
+      classes = "text-4xl leading-tight";
+      break;
+    case "h3":
+      classes = "text-3xl";
+      break;
+    case "h4":
+      classes = "text-2xl";
+      break;
+    default:
+      break;
+  }
+  return createElement(
+    type,
+    { className: `font-semibold ${classes} ${className}`, ...props },
+    children,
+  );
+}
