4639712813bf685c5036514b2cfefe5cfff3ee5f
aryan <aryan.me77@yahoo.com>
2023-11-08 19:40:09 +0330

    feat: dual the products page, change navbar

--- src/app/[lang]/(commerce)/products/page.tsx
+++ src/app/[lang]/(commerce)/products/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Heading from "@/components/atoms/Heading";
 import Products from "@/components/organs/Products";
-import { type Locale } from "@/lib/locale";
+import { getDictionary, type Locale } from "@/lib/locale";
 import prisma from "@/prisma/client";
 
 interface PropTypes {
@@ -12,17 +12,26 @@ interface PropTypes {
 }
 
 export default async function page({ params: { lang } }: PropTypes) {
-  const products = await prisma.product.findMany({
-    include: {
-      images: true,
+  const [dict, products] = await Promise.all([
+    getDictionary(lang),
+    prisma.product.findMany({
+      include: {
+        images: true,
+      },
+    }),
+  ]);
+
+  const {
+    pages: {
+      products: { description, title },
     },
-  });
+  } = dict;
 
   return (
     <section className="mt-32 mb-8 p-4 space-y-10 max-w-5xl mx-auto">
       <div className="text-center">
-        <Heading type="h2">Products</Heading>
-        <p>Order it for you or for your beloved ones </p>
+        <Heading type="h2">{title}</Heading>
+        <p>{description}</p>
       </div>
       <Products lang={lang} products={products} />
     </section>
