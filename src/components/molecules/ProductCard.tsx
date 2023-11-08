a1b31a2e664aac9c8bb36316b0df0641d1ea814b
aryan <aryan.me77@yahoo.com>
2023-11-08 19:32:45 +0330

    feat: add style price

--- src/components/molecules/ProductCard.tsx
+++ src/components/molecules/ProductCard.tsx
@@ -14,16 +14,20 @@ interface PropTypes {
 export default function ProductCard({ product, lang }: PropTypes) {
   return (
     <div className="shadow-sm rounded p-3 bg-white">
-      <div className="-mt-4 mb-2">
+      <div className="-mt-4 mb-2 h-[250px] overflow-hidden">
         <Image
           alt="sample product image"
           src={product.images[0]?.url || "/sample-product.png"}
           width={product.images[0]?.width || 3000}
           height={product.images[0]?.height || 2250}
+          className="w-full h-full object-cover"
         />
       </div>
       <p className="font-[500]">{product[`title_${lang}`]}</p>
-      <p className="rtl:text-left text-right">$9.99</p>
+      <p className="rtl:text-left text-right text-eprimary font-semibold">
+        $
+        {product.price}
+      </p>
     </div>
   );
 }
