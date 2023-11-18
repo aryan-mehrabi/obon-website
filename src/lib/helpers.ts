6fccb0a4c90d0d04fa7692820156675b70a6fa8e
aryan <aryan.me77@yahoo.com>
2023-11-19 01:53:14 +0330

    fix: handle redirect error

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -3,6 +3,8 @@
 import "server-only";
 
 import bcrypt from "bcrypt";
+import { isNotFoundError } from "next/dist/client/components/not-found";
+import { isRedirectError } from "next/dist/client/components/redirect";
 import * as z from "zod";
 
 import en from "@/dictionaries/en.json";
@@ -13,61 +15,56 @@ export const hashPassword = async (password: string): Promise<string> => {
   return bcrypt.hash(password, SALTROUND);
 };
 
-export const errorResponse = (message: string): ErrorResponse => {
-  return {
-    success: false,
-    message,
-  };
+export const errorResponse = (message: string): ErrorResponse => ({
+  success: false,
+  message,
+});
+
+export const successResponse = <T>(data: T | null = null): SuccessResponse => ({
+  success: true,
+  data,
+});
+
+export const serverActionMiddleware = <T extends (...args: any[]) => any>(fn: T) => async (...args: Parameters<T>): Promise<ErrorResponse | SuccessResponse> => {
+  try {
+    return await fn(...args);
+  } catch (error) {
+    if (isRedirectError(error) || isNotFoundError(error)) {
+      throw error;
+    }
+    if (error instanceof z.ZodError) {
+      return errorResponse(
+        error.issues.reduce(
+          (zodError, { message }) => `${zodError}\n ${message}`,
+          "",
+        ),
+      );
+    }
+    if (error instanceof Error) {
+      return errorResponse(error.message);
+    }
+    return errorResponse(en.errors.server);
+  }
 };
 
-export const successResponse = <T>(data: T | null = null): SuccessResponse => {
-  return {
-    success: true,
-    data,
-  };
-};
-
-export const serverActionMiddleware =
-  <T extends (...args: any[]) => any>(fn: T) =>
-  async (...args: Parameters<T>): Promise<ErrorResponse | SuccessResponse> => {
-    try {
-      return await fn(...args);
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        return errorResponse(
+export const apiMiddleware = <T extends (...args: any[]) => any>(fn: T) => async (...args: Parameters<T>): Promise<Response> => {
+  try {
+    return await fn(...args);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return Response.json(
+        errorResponse(
           error.issues.reduce(
             (zodError, { message }) => `${zodError}\n ${message}`,
             "",
           ),
-        );
-      }
-      if (error instanceof Error) {
-        return errorResponse(error.message);
-      }
-      return errorResponse(en.errors.server);
+        ),
+        { status: 400 },
+      );
     }
-  };
-
-export const apiMiddleware =
-  <T extends (...args: any[]) => any>(fn: T) =>
-  async (...args: Parameters<T>): Promise<Response> => {
-    try {
-      return await fn(...args);
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        return Response.json(
-          errorResponse(
-            error.issues.reduce(
-              (zodError, { message }) => `${zodError}\n ${message}`,
-              "",
-            ),
-          ),
-          { status: 400 },
-        );
-      }
-      if (error instanceof Error) {
-        return Response.json(errorResponse(error.message));
-      }
-      return Response.json(errorResponse(en.errors.server));
+    if (error instanceof Error) {
+      return Response.json(errorResponse(error.message));
     }
-  };
+    return Response.json(errorResponse(en.errors.server));
+  }
+};
