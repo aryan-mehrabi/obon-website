51c6ff7fbb7eae1496daa0b88612122525af020c
aryan <aryan.me77@yahoo.com>
2023-11-20 04:16:45 +0330

    feat: update skeleton

--- src/app/[lang]/(commerce)/cart/CartListItemSkeleton.tsx
+++ src/app/[lang]/(commerce)/cart/CartListItemSkeleton.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function CartListItemSkeleton() {
   return (
     <tr className="border-b-[1px]">
       <td className="flex items-center gap-1">
-        <div className="w-24 md:w-40 h-20 bg-slate-200 rounded m-2" />
-        <div className="h-6 w-24 bg-slate-200 rounded m-2" />
+        <Skeleton className="w-24 md:w-40 h-20 rounded m-2" />
+        <Skeleton className="h-6 w-24 rounded m-2" />
       </td>
       <td>
-        <div className="h-6 w-12 bg-slate-200 rounded m-2" />
+        <Skeleton className="h-6 w-24 rounded m-2" />
       </td>
       <td>
-        <div className="h-6 w-8 bg-slate-200 rounded m-2" />
+        <Skeleton className="h-6 w-8 rounded m-2" />
       </td>
       <td>
-        <div className="hidden md:table-cell h-6 w-12 bg-slate-200 rounded m-2" />
+        <Skeleton className="hidden md:table-cell h-6 w-24 rounded m-2" />
       </td>
     </tr>
   );
