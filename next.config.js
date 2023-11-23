06409da7fd1dd38a66808a9e6af31e1fb1ee9971
aryan <aryan.me77@yahoo.com>
2023-11-23 21:24:59 +0330

    feat: update to next 14

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
