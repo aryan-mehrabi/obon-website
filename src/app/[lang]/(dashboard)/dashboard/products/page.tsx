7a7e5aa2e3fd81a6d1f7c14ecee93d7eeedb3b1a
aryan <aryan.me77@yahoo.com>
2023-11-25 00:02:55 +0330

    feat: add fa to dashboard product page

--- src/app/[lang]/(dashboard)/dashboard/products/page.tsx
+++ src/app/[lang]/(dashboard)/dashboard/products/page.tsx
@@ -5,24 +5,35 @@ import Heading from "@/components/atoms/Heading";
 import Icon from "@/components/atoms/Icon";
 import { DataTable } from "@/components/organs/DataTable";
 import { Button } from "@/components/ui/button";
+import { getDictionary, Locale } from "@/lib/locale";
 import prisma from "@/prisma/client";
 
 import { columns } from "./columns";
 
-export default async function page() {
+export default async function page({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
   const products = await prisma.product.findMany({
     include: {
       images: true,
     },
   });
+  const dict = await getDictionary(lang);
+  const {
+    pages: {
+      dashboardProducts: { newProduct, title },
+    },
+  } = dict;
 
   return (
     <div className="space-y-4">
       <div className="flex justify-between">
-        <Heading type="h3">Products</Heading>
+        <Heading type="h3">{title}</Heading>
         <Button>
           <Icon render={PlusIcon} className="w-4 h-4" />
-          <p>New Product</p>
+          <p>{newProduct}</p>
         </Button>
       </div>
       <DataTable data={products} columns={columns} />
