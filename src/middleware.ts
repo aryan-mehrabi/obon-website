eed25198bfe5ee9608465c329d34b3b6b4171aef
aryan <aryan.me77@yahoo.com>
2023-12-12 19:03:55 +0330

    feat: add gaurds for dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,22 @@
 import { type NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
 import { getLocale, i18n } from "./lib/locale";
 
-export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
+const guardedRoutes = ["dashboard"];
+
+// eslint-disable-next-line consistent-return
+export async function middleware(request: NextRequest) {
+  const {
+    nextUrl: { pathname },
+  } = request;
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
-    locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
   );
-
-  // Redirect if there is no locale
   if (pathnameIsMissingLocale) {
+    // Redirect if there is no locale
     const locale = getLocale(request);
 
     // e.g. incoming request is /products
@@ -18,13 +24,22 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(
       new URL(
         `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
-        request.url
-      )
+        request.url,
+      ),
     );
+  }
+
+  const pathnameWithoutLocale = pathname.split("/").slice(2).join("/");
+  const locale = getLocale(request);
+  if (
+    guardedRoutes.some((route) => pathnameWithoutLocale.includes(route))
+    && !(await getToken({ req: request }))
+  ) {
+    return NextResponse.redirect(new URL(`/${locale}/login`, request.url));
   }
 }
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
 };
