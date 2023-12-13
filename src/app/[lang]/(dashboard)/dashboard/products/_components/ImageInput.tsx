2a5c553dee2328f7b9f79b21c49b92231401d7b2
aryan <aryan.me77@yahoo.com>
2023-12-13 04:37:32 +0330

    feat: add dnd for product images

---
+++ src/app/[lang]/(dashboard)/dashboard/products/_components/ImageInput.tsx
@@ -0,0 +1,151 @@
+import { ImageIcon, TrashIcon } from "@radix-ui/react-icons";
+import React, { useRef } from "react";
+import { UseFormReturn } from "react-hook-form";
+
+import Icon from "@/components/atoms/Icon";
+import DropZone from "@/components/molecules/DropZone";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import en from "@/dictionaries/en.json";
+import { ProductFirstStepFormSchema } from "@/types";
+
+interface PropTypes {
+  dict: typeof en;
+  form: UseFormReturn<ProductFirstStepFormSchema>;
+}
+
+export default function ImageInput({ dict, form }: PropTypes) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const {
+    pages: {
+      dashboardProducts: {
+        productForm: { images: imagesDict },
+      },
+    },
+  } = dict;
+
+  const images = form.watch("images");
+
+  const onClickDelete = (id: string | number) => {
+    const isDeletedDefault = images.find((image) => image.id === id)
+      ?.is_default;
+    const newImages = images.filter((image) => image.id !== id);
+    if (isDeletedDefault && newImages[0]) newImages[0].is_default = true;
+    form.setValue("images", newImages);
+  };
+
+  const onClickSetDefault = (id: string | number) => {
+    const newImages = images.map((image) => (image.id === id
+      ? { ...image, is_default: true }
+      : { ...image, is_default: false }));
+    form.setValue("images", newImages);
+  };
+
+  const onChangeFiles = (
+    eFiles: FileList,
+    {
+      onChange,
+      value,
+    }: { onChange: (e: typeof images) => void; value: typeof images },
+  ) => {
+    const files = Array.from(eFiles);
+    const imagesFile = files.map((file) => ({
+      id: file.name,
+      file,
+      is_default: false,
+    }));
+    if (!value.some((image) => image.is_default)) {
+      imagesFile[0].is_default = true;
+    }
+    onChange([...value, ...imagesFile]);
+  };
+  return (
+    <FormField
+      control={form.control}
+      name="images"
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      render={({
+        field: {
+          value, onChange, ref, ...field
+        },
+      }) => (
+        <FormItem className="col-span-2 ">
+          <FormLabel>{imagesDict.title}</FormLabel>
+          <FormControl>
+            <div className="grid grid-cols-3 gap-2">
+              {images.map((image) => (
+                <div key={image.id} className="relative">
+                  <div className="absolute top-0 right-0 rtl:left-0 m-1 flex items-center gap-1">
+                    {image.is_default && <Badge>Default</Badge>}
+                    <Button
+                      type="button"
+                      variant="destructive"
+                      className="p-1 w-6 h-6"
+                      onClick={() => onClickDelete(image.id)}
+                    >
+                      <Icon render={TrashIcon} className="w-4 h-4" />
+                    </Button>
+                  </div>
+                  <button
+                    type="button"
+                    className="w-full h-32 rounded overflow-hidden"
+                    onClick={() => onClickSetDefault(image.id)}
+                  >
+                    <img
+                      alt=""
+                      src={
+                        image.url ? image.url : URL.createObjectURL(image.file!)
+                      }
+                      className="w-full h-full object-cover"
+                    />
+                  </button>
+                </div>
+              ))}
+
+              <DropZone
+                onChange={(files) => {
+                  onChangeFiles(files, { value, onChange });
+                }}
+              >
+                <div className="mx-auto flex flex-col items-center justify-center text-center gap-2">
+                  <input
+                    ref={(input) => {
+                      inputRef.current = input;
+                      ref(input);
+                    }}
+                    placeholder="fileInput"
+                    className="hidden"
+                    type="file"
+                    multiple
+                    onChange={(e) => onChangeFiles(e.target.files!, { value, onChange })}
+                    accept="image/*"
+                    {...field}
+                  />
+                  <Icon render={ImageIcon} className="w-5 h-5" />
+                  <p className="text-sm text-muted-foreground">
+                    {imagesDict.placeholder}
+                  </p>
+                  <Button
+                    onClick={() => inputRef.current?.click()}
+                    variant="link"
+                    type="button"
+                  >
+                    {imagesDict.cta}
+                  </Button>
+                </div>
+              </DropZone>
+            </div>
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
--- src/app/[lang]/(dashboard)/dashboard/products/_components/WizardFirstStep.tsx
+++ src/app/[lang]/(dashboard)/dashboard/products/_components/WizardFirstStep.tsx
@@ -1,10 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { TrashIcon } from "@radix-ui/react-icons";
 import React from "react";
 import { useForm } from "react-hook-form";
 
-import Icon from "@/components/atoms/Icon";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { DialogClose, DialogFooter } from "@/components/ui/dialog";
 import {
@@ -25,6 +22,8 @@ import {
   ProductFormSchema,
 } from "@/types";
 
+import ImageInput from "./ImageInput";
+
 interface PropTypes {
   dict: typeof en;
   formData: ProductFormSchema;
@@ -43,9 +42,7 @@ export default function WizardFirstStep({
     pages: {
       dashboardProducts: {
         newProductModal,
-        productForm: {
-          name, price, quantity, images: imagesDict,
-        },
+        productForm: { name, price, quantity },
       },
     },
   } = dict;
@@ -60,27 +57,10 @@ export default function WizardFirstStep({
     },
   });
 
-  const images = form.watch("images");
-
-  const onClickDelete = (id: string | number) => {
-    const isDeletedDefault = images.find((image) => image.id === id)
-      ?.is_default;
-    const newImages = images.filter((image) => image.id !== id);
-    if (isDeletedDefault && newImages[0]) newImages[0].is_default = true;
-    form.setValue("images", newImages);
-  };
-
-  const onClickSetDefault = (id: string | number) => {
-    const newImages = images.map((image) => (image.id === id
-      ? { ...image, is_default: true }
-      : { ...image, is_default: false }));
-    form.setValue("images", newImages);
-  };
-
-  function onSubmit(values: ProductFirstStepFormSchema) {
+  const onSubmit = (values: ProductFirstStepFormSchema) => {
     setFormData((state) => ({ ...state, ...values }));
     setStep((step) => step + 1);
-  }
+  };
   return (
     <Form {...form}>
       {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
@@ -165,67 +145,7 @@ export default function WizardFirstStep({
               </FormItem>
             )}
           />
-          <FormField
-            control={form.control}
-            name="images"
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            render={({ field: { value, onChange, ...field } }) => (
-              <FormItem className="col-span-2">
-                <FormLabel>{imagesDict.title}</FormLabel>
-                <FormControl>
-                  <Input
-                    accept="image/*"
-                    multiple
-                    onChange={(e) => {
-                      const files = Array.from(e.target.files!);
-                      const imagesFile = files.map((file) => ({
-                        id: file.name,
-                        file,
-                        is_default: false,
-                      }));
-                      if (!value.some((image) => image.is_default)) {
-                        imagesFile[0].is_default = true;
-                      }
-                      onChange([...value, ...imagesFile]);
-                    }}
-                    type="file"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="col-span-2 grid grid-cols-3 gap-2">
-            {images.map((image) => (
-              <div key={image.id} className="relative">
-                <div className="absolute top-0 right-0 rtl:left-0 m-1 flex items-center gap-1">
-                  {image.is_default && <Badge className="">Default</Badge>}
-                  <Button
-                    type="button"
-                    variant="destructive"
-                    className="p-1 w-6 h-6"
-                    onClick={() => onClickDelete(image.id)}
-                  >
-                    <Icon render={TrashIcon} className="w-4 h-4" />
-                  </Button>
-                </div>
-                <button
-                  type="button"
-                  className="w-full h-24 rounded overflow-hidden"
-                  onClick={() => onClickSetDefault(image.id)}
-                >
-                  <img
-                    alt=""
-                    src={
-                      image.url ? image.url : URL.createObjectURL(image.file!)
-                    }
-                    className="w-full h-full object-cover"
-                  />
-                </button>
-              </div>
-            ))}
-          </div>
+          <ImageInput dict={dict} form={form} />
         </div>
         <DialogFooter>
           <DialogClose asChild>
