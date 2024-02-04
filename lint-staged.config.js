e7667f6e13a5944292f80ecf05290cb7e3ddb372
aryan <aryan.me77@yahoo.com>
2024-02-05 00:07:38 +0330

    chore: remove unnecessary step

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   "*": "prettier --ignore-unknown --write",
-  "*.{js,jsx,ts,tsx}": ["eslint --fix", "eslint"],
+  "*.{js,jsx,ts,tsx}": "eslint --fix",
   "**/*.ts?(x)": () => "npm run type-check",
 };
