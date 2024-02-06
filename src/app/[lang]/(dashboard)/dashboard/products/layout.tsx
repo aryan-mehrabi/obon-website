0b8b29ae537d8698054e6d350adf792d38086a96
aryan <aryan.me77@yahoo.com>
2024-02-06 23:38:31 +0330

    fix: tabs list active

--- src/app/[lang]/(dashboard)/dashboard/products/layout.tsx
+++ src/app/[lang]/(dashboard)/dashboard/products/layout.tsx
@@ -1,10 +1,9 @@
 import { Locale } from "@prisma/client";
-import { headers } from "next/headers";
-import Link from "next/link";
 import React from "react";
 
 import { getDictionary } from "@/lib/locale";
-import { cn } from "@/lib/utils";
+
+import TabsList from "./_components/TabsList";
 
 export default async function Layout({
   children,
@@ -13,35 +12,6 @@ export default async function Layout({
   children: React.ReactNode;
   params: { lang: Locale };
 }) {
-  const {
-    pages: {
-      dashboardProducts: { productRoutes },
-    },
-  } = await getDictionary(lang);
-
-  const headersList = headers();
-  const pathname = headersList.get("next-url") || "";
-  const subPath = pathname.split("/").filter((path) => path)[3] || "/";
-
-  return (
-    <>
-      <div className="mb-4 flex items-center">
-        {productRoutes.map(({ title, href, path }) => (
-          <Link
-            key={href}
-            href={href}
-            className={cn(
-              "flex h-7 items-center justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary",
-              path === subPath
-                ? "bg-muted font-medium text-primary"
-                : "text-muted-foreground",
-            )}
-          >
-            {title}
-          </Link>
-        ))}
-      </div>
-      {children}
-    </>
-  );
+  const dict = await getDictionary(lang);
+  return <TabsList dict={dict}>{children}</TabsList>;
 }
